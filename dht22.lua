3de664fbd36b1dae481608b52d0d1bb14c3f4372
Ievgen Duboriz <echemistinua@gmail.com>
2016-10-18 15:10:01 +0300

    Update dht22.lua

--- dht22.lua
+++ dht22.lua
@@ -4,7 +4,7 @@ local status, temp, humi, temp_dec, humi_dec = dht.read(pin)
 if status == dht.OK then
     -- подготовка строчных переменных для модулей server и thingspeak
     h = (math.floor(humi)).."."..(humi_dec/100)
-    t = (math.floor(temp)).."."..(temp_dec/100)
+    t = (math.floor(temp)).."."..(math.abs(temp_dec/100))
     print(string.format("t = %s;h = %s",t,h))
 
 elseif status == dht.ERROR_CHECKSUM then
