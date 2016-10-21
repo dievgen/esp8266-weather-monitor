e65e82265ac6c452d6bc4d27f5aa22eea9f636a2
Ievgen Duboriz <echemistinua@gmail.com>
2016-10-21 13:41:01 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
 wifi.setmode(wifi.STATION)
-print("Подключаемся к сети %название_сети%")
-wifi.sta.config("%название_сети%","%пароль_сети%")
+print("Connecting to the SSID")
+wifi.sta.config("SSID","password")
 wifi.sta.connect()
 wifi.sta.setip({ip="192.168.0.100",netmask="255.255.255.0",gateway="192.168.0.1"})
 
-print("Настройка ESP8266: " ..wifi.getmode())
-print("MAC адрес модуля: " ..wifi.ap.getmac())
-print("IP модуля "..wifi.sta.getip())
+print("ESP8266 mode: " ..wifi.getmode())
+print("ESP8266 MAC: " ..wifi.ap.getmac())
+print("ESP8266 IP :"..wifi.sta.getip())
 
 dofile("server.lua")
 collectgarbage()
